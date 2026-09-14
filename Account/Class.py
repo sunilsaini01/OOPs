2c785b2cb437966c617982cdf7cf98f49cca810e
sunil <kumarsunil100403@gmail.com>
2026-09-14 16:17:41 +0530

    Add Account class with debit and credit methods

---
+++ Account/Class.py
@@ -0,0 +1,26 @@
+class Account:
+
+    def __init__(self, balance, account_number):
+        self.balance = balance
+        self.account_number = account_number
+
+    def debit(self, amount):
+        if amount <= self.balance:
+            self.balance = self.balance - amount
+            print("Amount debit successfully", amount)
+            print("Remaining Balance", self.balance)
+            
+        else:
+            print("insufficient balance")
+        
+
+    def credit(self, amount):
+        if amount >= 0:
+            self.balance = self.balance + amount
+            
+            print("Credited amount", amount)
+            print("Updated balance", self.balance)
+            
+            
+account = Account(10000, 101)
+account.credit(2000)
